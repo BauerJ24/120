301b5e66137ba4d02ed3acbc486979bb0e6bfd25
Yuqiao Jiang <yujiang@ucsc.edu>
2017-05-23 02:47:25 -0700

    0523

--- Rhinoceromance/js/main.js
+++ Rhinoceromance/js/main.js
@@ -1,7 +1,7 @@
 // Mistakes into Miracles
 //initialize game
 
-var game = new Phaser.Game(1000, 800, Phaser.AUTO);
+var game = new Phaser.Game(1280, 720, Phaser.AUTO);
 //add states
 game.state.add("Preloader", Preloader);
 game.state.add("Mainmenu", Mainmenu);
@@ -12,11 +12,12 @@ game.state.add("Gameover",Gameover);
 game.state.start("Preloader");
 
 var day=1;
-var charm=10;
-var fitness=10;
-var smarts=10;
-var style=10;
-var stress=0;
-var money=0;
+
+var player = {};
+player.charm=10;
+player.fitness=10;
+player.smarts=10;
+player.style=10;
+player.stress=0;
 
 var actiontaken=0;
